a4378b5d1026a32029c3d78df86e0af67ea21773
王稳健 <420655170@qq.com>
2023-04-20 07:49:49 +0800

    使用lowdb动态添加删除数据

--- 10.记账本案例/routes/index.js
+++ 10.记账本案例/routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+// 导入lowdb
+const lowdb = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync(__dirname + '/../data/db.json')
+// 获取db对象
+const db = lowdb(adapter)
+// 引入shortid
+const shortid = require('shortid')
+
 // 记账本列表
 router.get('/account', function (req, res, next) {
   res.render('list');
@@ -15,6 +24,10 @@ router.get('/account/create', (req, res, next) => {
 router.post('/account', (req, res, next) => {
   // 获取请求体的数据
   console.log(req.body)
+  // shortid生成id
+  const id = shortid.generate()
+  // 将数据保存到db.json中
+  db.get('account').unshift({ id: id, ...req.body }).write()
   res.send('新增记录')
 })
 
